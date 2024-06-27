da6805cc5de6ef8364842dc439fa5d2981245cdc
Nicolas Deslandes <ndeslandes@live.fr>
2024-06-28 00:47:23 +0100

    Cleanup all style issues

--- tests/Chr.Avro.Json.Tests/RecordSerializationTests.cs
+++ tests/Chr.Avro.Json.Tests/RecordSerializationTests.cs
@@ -293,8 +293,6 @@ namespace Chr.Avro.Serialization.Tests
             var t = textReader.ReadToEnd();
 
             var reader = new Utf8JsonReader(stream.ToArray());
-            
-
             var with = deserialize(ref reader);
 
             Assert.Null(with.Second);
